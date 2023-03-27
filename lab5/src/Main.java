59fc2fe7508f4386294afad6467463db52df95bb
Ryunyy <khirigiri@gmail.com>
2023-03-27 22:41:32 +0700

    add comments

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -6,21 +6,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //порождающая матрица
         ArrayList<int[]> GMatrix;
+        //базис. пранировалось либо приводить к этому базису, либо вычислять его с помощью слау...пока не нужен
         int[] basis = {0, 1, 2};
+        //массив кодов. уже прям кодов кодов, т.е. с контрольными битами
         ArrayList<String> codes;
+        //переменные всякие, за себя говорящие
         int wordsNum, n, d;
+        //поменяй путь к файлику
         String path = "D:\\Study\\4Rehc\\TI\\lab5\\TI_Last\\lab5\\code.txt";
-
+        //считываем матрикс в матрикс
         GMatrix = fileReader(path);
+        //тут она должна была преобразовываться
         //matrixRevork(GMatrix, basis);
-
+        //размерность кода
         n = GMatrix.size();
+        //максимум кол-во слов для кодирования
         wordsNum = (int)Math.pow(2, n);
 
         System.out.println("Code dimension: " + n);
         System.out.println("Maximal possible code words: " + wordsNum);
+        //генерация кодов поменбше и кодов побольбше
         codes = codeWordGenerator(GMatrix, n);
+        //вычисление мин расстояния между кодами
         d = minimalDistance(codes);
         System.out.println("Minimal code distance: " + d);
         System.out.println("\nGenerative matrix:");
@@ -31,6 +40,7 @@ public class Main {
         int toReturn = Integer.MAX_VALUE, dist;
         for(int i = 0; i < codes.size()-1; i++)
             for(int j = i+1; j < codes.size(); j++){
+                //функция сравнения двух кодов
                 dist = distance(codes.get(i), codes.get(j));
                 //System.out.println(codes.get(i) + " | " + codes.get(j) + " -> " + dist);
                 toReturn = dist < toReturn? dist: toReturn;
@@ -39,6 +49,7 @@ public class Main {
     }
 
     private static int distance(String first, String second){
+        //сложение происходит только если разряды отличаются, это и есть отличающаяся часть
         int toReturn = 0;
         for(int i = 0; i < first.length(); i++){
             if(first.charAt(i) != second.charAt(i))
@@ -48,11 +59,14 @@ public class Main {
     }
 
     public static ArrayList<String> codeWordGenerator(ArrayList<int[]>matrix, int n){
+        //первый - для кодов с контрольными битами, второй - для обычных
         ArrayList<String> codeWords = new ArrayList<>(), codes;
+        //тут сами коды, ну т.е. 000 001 и т.д. к примеру, если n = 3
         codes = codeGenerator(n);
         int wordNum = (int)Math.pow(2, n);
 
         for(int i = 0; i < wordNum; i++) {
+            //умножаем код на матрицу - получаем код с контрольными битами
             codeWords.add(codeMatrixMultiplier(codes.get(i), matrix));
             System.out.println("Code: " + codes.get(i) + " | code word:" + codeWords.get(i));
         }
@@ -66,8 +80,10 @@ public class Main {
         String code;
         for(int i = 0; i < wordsCount; i++){
             if(i == 0)
+                //для первого - 000 к прим. одтельно задается
                 code = setZero(n);
             else
+                //к предыдущему ксором плюсуется единичка
                 code = xorPlusOne(codeWords.get(i-1));
             codeWords.add(code);
         }
@@ -75,6 +91,7 @@ public class Main {
     }
 
     private static String setZero(int n){
+        //ну тут все просто
         String toReturn = "";
         for(int i = 0; i < n; i++)
             toReturn += "0";
@@ -82,9 +99,11 @@ public class Main {
     }
 
     private static String xorPlusOne(String previous){
+        //мемои - перегруз, т.е. при сложении 1+1 ответ 0, но 1 в уме. вот этот "в уме" и есть мемори
         StringBuilder code = new StringBuilder(previous);
         int memory = 0;
 
+        //для последней цифры считается отдельно, т.к. она порождает мемори
         if(code.charAt(previous.length()-1) == '1') {
             code.setCharAt(previous.length()-1, '0');
             memory = 1;
@@ -93,6 +112,7 @@ public class Main {
             code.setCharAt(previous.length()-1, '1');
         }
 
+        //ну и от конца к началу мы этот "в уме" раскидывем по числу, куда можем
         for(int pos = previous.length()-2; pos >= 0; pos--){
             if(memory == 1){
                 if(code.charAt(pos) == '1')
@@ -109,13 +129,14 @@ public class Main {
     private static String codeMatrixMultiplier(String codeWord, ArrayList<int[]>matrix){
         String toReturn = "";
         int sum;
-
+        //суть проста - складываются только те элементы, где и в коде 1, и где в матрице 1. остальное 0. такая магия
         for(int i = 0; i < matrix.get(0).length; i++){
             sum = 0;
             for(int j = 0; j < codeWord.length(); j++){
                 if(codeWord.charAt(j) == '1' && matrix.get(j)[i] == 1)
                     sum++;
             }
+            //а тут по сумме получаем, что писать
             if(sum % 2 != 0)
                 toReturn += "1";
             else
@@ -125,19 +146,24 @@ public class Main {
     }
 
     public static ArrayList<int[]> fileReader(String path) throws IOException {
+        //буффер, строка для чтения линии, список параметров - ну типо эти 3 5 к примеру в начале файла. сплитятся по пробелу
         BufferedReader file = new BufferedReader(new FileReader(path));
         String params = file.readLine();
         List<String> parameter = List.of(params.split(" "));
         int height = Integer.valueOf(parameter.get(0)), width = Integer.valueOf(parameter.get(1));
 
         String line;
+        //массив массивов. ну типо матрица
         ArrayList<int[]> toReturn = new ArrayList<>();
         for(int i = 0; i < height; i++){
+            //считали строку
             line = file.readLine();
             int[] row = new int[width];
             for(int j = 0; j < width; j++){
+                //расхерачили строку на букавы и запихали ету сю пакостб в ряды. кста, числа, не символы
                 row[j] = Integer.valueOf(Character.toString(line.charAt(j)));
             }
+            //грузим контейнеры с букавами в матрицу
             toReturn.add(row);
         }
 
@@ -145,6 +171,7 @@ public class Main {
     }
 
     public static void matrixOutput(ArrayList<int[]> matrix){
+        //пакостим в консольку букавами из контейнеров
         for (int[] row: matrix) {
             System.out.println(Arrays.toString(row));
         }
